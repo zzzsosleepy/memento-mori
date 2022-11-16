b87a452d8a3ac83ae99fb922655bc851ac2c32ae
Jeffrey Chipman <zzzsojeffrey@gmail.com>
2022-11-15 21:37:25 -0400

    Added Google authentication

--- src/App.js
+++ src/App.js
@@ -2,13 +2,23 @@ import './App.css';
 import Home from './pages/home.jsx';
 import Navbar from './components/Navbar/navbar.jsx';
 import Hand from './components/Hand/hand.jsx';
+// import state
+import React, { useState } from 'react';
 
 function App() {
+  // State for which page to display
+  const [page, setPage] = useState('home');
+
+  function loginSuccessful(userEmail) {
+    console.log('Login Successful by ' + userEmail);
+    setPage('hand');
+    console.log(page);
+  }
   return (
     <div className="App">
       <Navbar />
-      {/* <Home /> */}
-      <Hand />
+      {page === 'home' ? <Home loginSuccessful={loginSuccessful} /> : null}
+      {page === 'hand' ? <Hand /> : null}
     </div>
   );
 }
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -2,35 +2,71 @@ import React, { useRef } from 'react';
 import { firestore } from '../firebase';
 import { addDoc, collection } from '@firebase/firestore';
 import './home.css';
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import { ImGoogle3 } from 'react-icons/im';
 
-export default function Home() {
+
+
+export default function Home({ loginSuccessful }) {
     const messageRef = useRef();
     const ref = collection(firestore, "messages");
 
-    const handleSave = async (e) => {
-        e.preventDefault();
-        let data = {
-            message: messageRef.current.value,
-        };
+    // const handleSave = async (e) => {
+    //     e.preventDefault();
+    //     let data = {
+    //         message: messageRef.current.value,
+    //     };
 
-        try {
-            addDoc(ref, data);
-        } catch (e) {
-            console.log(e);
-        }
+    //     try {
+    //         addDoc(ref, data);
+    //     } catch (e) {
+    //         console.log(e);
+    //     }
+    // }
+
+    // Handle firebase authentication with google
+    function handleSignin(e) {
+        e.preventDefault();
+        const auth = getAuth();
+        const provider = new GoogleAuthProvider();
+        signInWithPopup(auth, provider)
+            .then((result) => {
+                // This gives you a Google Access Token. You can use it to access the Google API.
+                const credential = GoogleAuthProvider.credentialFromResult(result);
+                const token = credential.accessToken;
+                // The signed-in user info.
+                const user = result.user;
+                console.log(user.email + " signed in");
+                // call the loginSuccess function to set the page state
+                loginSuccessful(user.email);
+                // ...
+            }).catch((error) => {
+                // Handle Errors here.
+                const errorCode = error.code;
+                const errorMessage = error.message;
+                // The email of the user's account used.
+                const email = error.customData.email;
+                // The AuthCredential type that was used.
+                const credential = GoogleAuthProvider.credentialFromError(error);
+                // ...
+            });
     }
+
+
+
     return (
         <div className="home-page">
             <div className="dark-overlay"></div>
             <h2>Welcome to</h2>
             <h1>💀 Memento Mori 💀</h1>
             <hr className="line-separator" />
-            <form onSubmit={handleSave}>
-                <label>Email Address</label>
+            <form onSubmit={handleSignin}>
+                {/* <label>Email Address</label>
                 <input type="text" ref={messageRef} />
                 <label>Password</label>
-                <input type="password" ref={messageRef} />
-                <button type="submit">Login</button>
+            <input type="password" ref={messageRef} /> */}
+                <label>Log In</label>
+                <button type="submit"><ImGoogle3 className="googleLogo" /> Sign in with Google</button>
                 <hr />
                 {/* Don't have an account? Sign up */}
                 <label>Don't have an account?</label>
